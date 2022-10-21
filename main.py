530486aedf69f0a493aeb483cbb702f10defb149
Stefano <49915584+stefanoppp@users.noreply.github.com>
2022-10-21 12:25:33 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import cv2
 from back_propagation import Back_Propagation
 
 def main():
   fotos=['1A58099.jpg','1B58099.jpg','2A58099.jpg','2B58099.jpg','3A58099.jpg','3B58099.jpg','4A58099.jpg','4B58099.jpg','5A58099.jpg','5B58099.jpg']
   
-  iteraciones=int(input("Digite cantidad de iteraciones: "))
+  for foto in fotos:
+    image = cv2.imread(foto)
+    for i in range(len(image)):
+      print(image[i][0][0])
 
-  
-  
   
 main()
