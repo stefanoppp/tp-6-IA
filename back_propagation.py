23798a7b981286ff188cf28cefb034e73d3ef8fd
Stefano <49915584+stefanoppp@users.noreply.github.com>
2022-10-21 12:10:23 -0300

    Add files via upload

---
+++ back_propagation.py
@@ -0,0 +1,77 @@
+from neurona_final import Neurona_final
+from neuronas_ocultas import Neurona_oculta
+import matplotlib.pyplot as plt
+import random
+
+class Back_Propagation():
+    
+    def __init__(self,entradas,salidas):
+        self.entradas=entradas
+        self.salidas=salidas
+    
+    def main(self):
+        
+        cant_neuronas=int(input("Digite cantidad de neuronas: "))
+        iteraciones=int(input("Digite cantidad de iteraciones: "))
+        
+        neuronas=[]
+        # generamos neuronas ocultas con 7680 pesos c/u
+        for i in range(cant_neuronas):
+            pesos_neuronales=[]
+            for j in range(7680):
+                peso_random=random.random()
+                pesos_neuronales.append(peso_random)
+            n=Neurona_oculta(pesos_neuronales)
+            neuronas.append(n)
+            
+        # generamos neurona final con 1 peso por cada neurona generada
+        pesos_finales=[]
+        for j in range(len(neuronas)):
+            peso_random=random.random()
+            pesos_finales.append(peso_random)
+        nf=Neurona_final(pesos_finales)
+
+        # ------------------comienza la iteracion------------------
+        errores=[]
+        # obtenemos errores neurona final y red
+        for iteracion in range(iteraciones):
+            for i in range(len(self.entradas)):
+                salidas_ocultas=[]
+                for neurona in neuronas:
+                    salida_oculta=neurona.obtener_salida(self.entradas[i])
+                    salidas_ocultas.append(salida_oculta)
+                
+                salida_red=nf.obtener_salida(salidas_ocultas)
+                error_red=nf.obtener_error(self.salidas[i],salida_red)
+                errores.append(error_red)
+        # ---------------------obtenemos errores neuronas ocultas--------------
+                errores_ocultos=[]        
+                for j in range(len(neuronas)):
+                    error_oculto=neuronas[j].obtener_error(nf.pesos[j],error_red,salidas_ocultas[j])
+                    errores_ocultos.append(error_oculto)
+                    
+        # ---------------------------recalculamos pesos ocultos--------------
+                for j in range(len(neuronas)):
+                    neuronas[j].calcular_nuevos_pesos(self.entradas[i],errores_ocultos[j])        
+
+        # -----------------------recalculamos pesos finales-------------------
+                nf.calcular_nuevos_pesos(error_red,salidas_ocultas)
+        print("Pesos ocultos actualizados: ")
+        for neurona in neuronas:
+            print(neurona.pesos)
+        print("Pesos finales actualizados: ")
+        print(nf.pesos)
+        
+        array=[]
+        for i in range(len(self.entradas)):
+            array.append([])
+        
+        j=0
+        for i in range(len(errores)):
+            array[j].append(errores[i])
+            j+=1
+            if j==10:
+                j=0
+        for element in array:
+            plt.plot(element)
+        plt.show()
---
+++ neuronas_ocultas.py
@@ -0,0 +1,24 @@
+import math
+import numpy as np
+class Neurona_oculta:
+    def __init__(self,pesos):
+        self.pesos=pesos
+        self.lr=0.4
+        
+    def obtener_salida(self,entradas):
+        prod_escalar=np.dot(self.pesos,entradas)
+        salida_real=self.sigmoidea(prod_escalar)
+        return salida_real
+    
+    def obtener_error(self,peso_entrada_neurona_final,error_red,salida_neurona):
+        error=salida_neurona*(1-salida_neurona)*(peso_entrada_neurona_final*error_red)
+        return error
+        
+    def calcular_nuevos_pesos(self,entradas,error_neurona):
+        for i in range(len(self.pesos)):
+            self.pesos[i]=self.pesos[i]+(self.lr*entradas[i]*error_neurona)
+        return self.pesos
+
+    def sigmoidea(self,prod_escalar):
+        sig = 1 / (1 + math.exp(-prod_escalar))
+        return sig
